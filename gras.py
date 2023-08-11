145a56ca4ff53ae5fc0f0f6528d3f3f6737f8da0
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-11 15:21:36 +0300

    Update gras.py

--- gras.py
+++ gras.py
@@ -177,7 +177,8 @@ if uploaded_file is not None:
     elif chart_select == "Brief Description of Data Frame":
         # st.write(df)
         num_claims = len(df["Claim No"])
-        st.markdown(f"**Total number of claims:** {num_claims}")
+        #st.markdown(f"**Total number of claims:** {num_claims}")
+        
         claims_per_type = df.groupby("Claim Type").size()
         newclaims = claims_per_type.to_html(index=False)
         st.markdown(newclaims, unsafe_allow_html=True)
