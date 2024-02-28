8ab1cb30bd955b5c8e9e2d46af19db2fcf5c0cd6
Collins Chetekei <112887160+chetekei@users.noreply.github.com>
2024-02-28 11:36:08 +0300

    Update gras.py

--- gras.py
+++ gras.py
@@ -34,80 +34,85 @@ uploaded_file = st.sidebar.file_uploader(
 if uploaded_file is not None:
     try:
         if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-            df = pd.read_excel(uploaded_file, header=8)
+            data_types = {'Policy No': str}
+            df = pd.read_excel(uploaded_file, dtype = data_types, header=6)
+                      
         elif uploaded_file.type == "text/csv":
-            df = pd.read_csv(uploaded_file, header=8)
-    except Exception as e:
+            df = pd.read_csv(uploaded_file, header=6)
+
+    except Exception as e:    
         st.write("Error:", e)
+
+if uploaded_file is not None:
 
                       
 
-# convert date column to month name
-df = df.iloc[1:]
-df['Month'] = pd.to_datetime(df['Loss Date']).dt.strftime('%B')
-df['Day'] = pd.to_datetime(df['Loss Date']).dt.day_name()
-df['Year'] = pd.to_datetime(df['Loss Date']).dt.year
-df.dropna(subset=['Claim No'], inplace=True)
-mask = df['Claim Type'].str.startswith('Work Injury')
-df.loc[mask, 'Claim Type'] = 'WIBA'
-df['Count'] = 1
-
-df['Frequency'] = np.bool_(1)
-
-# convert timestamp to datetime
-df['timestamp'] = pd.to_datetime(df['Time of Loss'])
-
- # Main Streamlit app code
-def main(): 
-    # Create a sidebar to switch between views
-    view = st.sidebar.radio("Select", ["Dashboard"])
-
-    if view == "Dashboard":
-
-        months = df.groupby('Month')['Count'].sum()
-        day = df.groupby('Day')['Count'].sum()
-        claims = df.groupby('Claim Type')['Count'].sum()
-
-        # Sort by the sum of Count in descending order
-        result = months.sort_values(by='Count', ascending=False)
-        result1 = day.sort_values(by='Count', ascending=False)        
-        result2 = claims.sort_values(by='Count', ascending=False)
-
-        # Get the class with the highest sum
-        highest_month = result.head(1)
-        highest_day = result1.head(1)
-        highest_class = result2.head(1)
-
-        month_name = highest_month['Month'].values[0]
-        day_name = highest_day['Day'].values[0]
-        class_name = highest_class['Claim Type'].values[0]
-
-
-
-        st.markdown(
-                        f'<div style= "display: flex; flex-direction: row;">'  # Container with flex layout
-                        f'<div style="background-color: #f19584; padding: 10px; border-radius: 10px; width: 250px; margin-right: 20px;">'
-                        f'<strong style="color: black; font-size: 12px">MOST FREQUENT CLAIM TYPE</strong> <br>'  
-                        f"<br>"
-                        f"{class_name}<br>"
-                        f'</div>'
-                        f'<div style="background-color: #FFE599; padding: 10px; border-radius: 10px; width: 250px; margin-right: 20px;">'
-                        f'<strong style="color: black; font-size: 12px">MOST FREQUENT CLAIM MONTH</strong> <br>'
-                        f"<br>"
-                        f"{month_name}<br>"
-                        f'</div>'                
-                        f'<div style="background-color: #a8e4a0; padding: 10px; border-radius: 10px; width: 250px; margin-right: 20px;">'
-                        f'<strong style="color: black; font-size: 12px">MOST FREQUENT DAY</strong> <br>'  
-                        f"<br>"
-                        f"{day_name}<br>"
-                        f'</div>'                    
-                        f'</div>',
-                        unsafe_allow_html=True
-                    )
-
-               
-          
-   
-
-
+    # convert date column to month name
+    df = df.iloc[1:]
+    df['Month'] = pd.to_datetime(df['Loss Date']).dt.strftime('%B')
+    df['Day'] = pd.to_datetime(df['Loss Date']).dt.day_name()
+    df['Year'] = pd.to_datetime(df['Loss Date']).dt.year
+    df.dropna(subset=['Claim No'], inplace=True)
+    mask = df['Claim Type'].str.startswith('Work Injury')
+    df.loc[mask, 'Claim Type'] = 'WIBA'
+    df['Count'] = 1
     
+    df['Frequency'] = np.bool_(1)
+    
+    # convert timestamp to datetime
+    df['timestamp'] = pd.to_datetime(df['Time of Loss'])
+    
+     # Main Streamlit app code
+    def main(): 
+        # Create a sidebar to switch between views
+        view = st.sidebar.radio("Select", ["Dashboard"])
+    
+        if view == "Dashboard":
+    
+            months = df.groupby('Month')['Count'].sum()
+            day = df.groupby('Day')['Count'].sum()
+            claims = df.groupby('Claim Type')['Count'].sum()
+    
+            # Sort by the sum of Count in descending order
+            result = months.sort_values(by='Count', ascending=False)
+            result1 = day.sort_values(by='Count', ascending=False)        
+            result2 = claims.sort_values(by='Count', ascending=False)
+    
+            # Get the class with the highest sum
+            highest_month = result.head(1)
+            highest_day = result1.head(1)
+            highest_class = result2.head(1)
+    
+            month_name = highest_month['Month'].values[0]
+            day_name = highest_day['Day'].values[0]
+            class_name = highest_class['Claim Type'].values[0]
+    
+    
+    
+            st.markdown(
+                            f'<div style= "display: flex; flex-direction: row;">'  # Container with flex layout
+                            f'<div style="background-color: #f19584; padding: 10px; border-radius: 10px; width: 250px; margin-right: 20px;">'
+                            f'<strong style="color: black; font-size: 12px">MOST FREQUENT CLAIM TYPE</strong> <br>'  
+                            f"<br>"
+                            f"{class_name}<br>"
+                            f'</div>'
+                            f'<div style="background-color: #FFE599; padding: 10px; border-radius: 10px; width: 250px; margin-right: 20px;">'
+                            f'<strong style="color: black; font-size: 12px">MOST FREQUENT CLAIM MONTH</strong> <br>'
+                            f"<br>"
+                            f"{month_name}<br>"
+                            f'</div>'                
+                            f'<div style="background-color: #a8e4a0; padding: 10px; border-radius: 10px; width: 250px; margin-right: 20px;">'
+                            f'<strong style="color: black; font-size: 12px">MOST FREQUENT DAY</strong> <br>'  
+                            f"<br>"
+                            f"{day_name}<br>"
+                            f'</div>'                    
+                            f'</div>',
+                            unsafe_allow_html=True
+                        )
+    
+                   
+              
+       
+    
+    
+        
